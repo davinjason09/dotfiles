79728c38aa98dac772f86c2282ad8983782cf867
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-07-19 00:38:29 +0700

    refactor: made _PSFZF_FZF_DEFAULT_OPTS the same as FZF_DEFAULT_OPTS

--- Documents/PowerShell/Scripts/Personal/fzf.ps1
+++ Documents/PowerShell/Scripts/Personal/fzf.ps1
@@ -11,18 +11,7 @@ $env:FZF_DEFAULT_OPTS = @"
 --bind ctrl-e:toggle-preview
 "@
 
-$env:_PSFZF_FZF_DEFAULT_OPTS = @"
---color=fg:#E4F0FB,fg+:#FFFFFF,bg:-1,bg+:#262626
---color=hl:#91B4D5,hl+:#88DDFF,info:#FFDC96,marker:#AAE682
---color=prompt:#CF679D,spinner:#5DE4C7,pointer:#9696FF,header:#5FB3A0
---color=gutter:#121212,border:#42675A,scrollbar:#5DE4C7,preview-scrollbar:#5DE4C7
---color=label:#aeaeae,query:#FFFFFF
---cycle --height=50% --border="rounded"
---prompt=" " --marker=" " --pointer=" "  
---separator="─" --scrollbar="│"
---bind alt-w:toggle-preview-wrap
---bind ctrl-e:toggle-preview
-"@
+$env:_PSFZF_FZF_DEFAULT_OPTS = $env:FZF_DEFAULT_OPTS
 
 $env:FZF_ALT_C_COMMAND = "fd --type d --hidden --follow --exclude .git --fixed-strings --strip-cwd-prefix --color always"
 $env:FZF_ALT_C_OPTS = @"
