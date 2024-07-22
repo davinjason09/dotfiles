35bdca4e424fc946a577846600d276845d984fff
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-07-23 00:06:55 +0700

    feat: configured komorebi

---
+++ Documents/PowerShell/Scripts/Personal/komorebi.ps1
@@ -0,0 +1,17 @@
+# Function to trigger komorebi
+
+function Start-Komorebi {
+  if (! (Get-Process "komorebi" -ErrorAction SilentlyContinue)) {
+    komorebic.exe start  
+  }
+  if (! (Get-Process "AutoHotKey" -ErrorAction SilentlyContinue)) {
+    # AutoHotkey.exe "C:/Users/kevinnitro/.config/komorebi/komorebi.ahk"
+    AutoHotkey.exe "$env:USERPROFILE/.config/autohotkey.ahk"
+  }
+}
+
+# Update applications.yml
+
+function Update-KomorebiApplications {
+  komorebic fetch-asc
+}
