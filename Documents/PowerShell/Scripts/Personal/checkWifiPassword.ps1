c47e154c5c58127c3210e440afdd187024f5c1b5
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-07-24 23:14:16 +0700

    refactor(check-wifi): improve readability

--- Documents/PowerShell/Scripts/Personal/checkWifiPassword.ps1
+++ Documents/PowerShell/Scripts/Personal/checkWifiPassword.ps1
@@ -1,13 +1,15 @@
+# Ref = https://github.com/KevinNitroG/windows-dotfiles/blob/main/Documents/PowerShell/Scripts/kevinnitro/checkWifiPassword.ps1
 function Check-WiFiPassword {
   param (
     [string]$name = $null
   )
+
   if (-not $name) {
-		if (Get-Command fzf) {
-			$name = netsh wlan show profiles |
-				Select-String -Pattern "All User Profile\s+:\s+(.*)" |
-				ForEach-Object { $_.Matches.Groups[1].Value } |
-				fzf --prompt="Select Wi-Fi  " --height=~80% --layout=reverse --border --exit-0 --cycle --margin="2,40" --padding=1
+		if (Get-Command fzf -ErrorAction SilentlyContinue) {
+			$name = netsh wlan show profiles 
+				| Select-String -Pattern "All User Profile\s+:\s+(.*)"
+				| ForEach-Object { $_.Matches.Groups[1].Value } 
+				| fzf --prompt="Select Wi-Fi  " --height=~80% --layout=reverse --border=rounded --exit-0 --cycle --margin="2,40" --padding=1
     } else {
 			"LIST OF SAVED WI-FI"
 			"`n---`n"
@@ -18,11 +20,12 @@ function Check-WiFiPassword {
 			for ($i = 0; $i -lt $wifiList.Length; $i++) {
 				"{0,5}: {1}" -f ($i+1), $wifiList[$i]
 			}
+
 			"`n---`n"
 
 			$inputPosition = Read-Host "Enter the number of the Wi-Fi network you want to check the password (Enter for current network)"
 
-			if ([string]::IsNullOrEmpty($inputPosition)) {
+			if ([String]::IsNullOrEmpty($inputPosition)) {
 				$name = ((netsh wlan show interfaces | Select-String -Pattern "Profile" -Context 0,1) -split ":")[1].Trim()
 			} else {
 				$index = [int]$inputPosition - 1
@@ -43,11 +46,17 @@ function Check-WiFiPassword {
 
 	$wlanProfile = netsh wlan show profile name="$name" key=clear
 	$password = $wlanProfile | Select-String -Pattern "Key Content\s+:\s+(.*)" | ForEach-Object { $_.Matches.Groups[1].Value }
-	"Wi-Fi network: $name"
+	Write-Host "Wi-Fi network: $name"
 
 	if ($password) {
-		"Password: $password"
+		Write-Host "Password: $password"
 	} else {
-		"No password found."
+		Write-Host "No password found."
+	}
+}
+	if ($password) {
+		Write-Host "Password: $password"
+	} else {
+		Write-Host "No password found."
 	}
 }
