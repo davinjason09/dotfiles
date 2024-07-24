f253368b8c16cf15ac645dab15a0116e521f88e4
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2024-07-24 23:15:23 +0700

    style(psreadline): use catppuccin color theme

--- Documents/PowerShell/Scripts/Personal/PSReadline.ps1
+++ Documents/PowerShell/Scripts/Personal/PSReadline.ps1
@@ -1,17 +1,28 @@
-$ScriptBlock = {
-  Param([string]$line)
-  if ($line -like " *") {
-    return $false
-  } 
+# Ref = https://github.com/catppuccin/powershell
+$Colors = @{
+	# Powershell colours
+	ContinuationPrompt     = $Flavor.Teal.Foreground()
+	Emphasis               = $Flavor.Red.Foreground()
+	Selection              = $Flavor.Surface0.Background()
+	
+	# PSReadLine prediction colours
+	InlinePrediction       = $Flavor.Overlay0.Foreground()
+	ListPrediction         = $Flavor.Mauve.Foreground()
+	ListPredictionSelected = $Flavor.Surface0.Background()
 
-  $ignorePSReadline = @("user", "pass", "account")
-  foreach ($ignore in $ignorePSReadline) {
-    if ($line -match $ignore) {
-      return $false
-    }
-  }
-
-  return $true
+	# Syntax highlighting
+	Command                = $Flavor.Blue.Foreground()
+	Comment                = $Flavor.Overlay0.Foreground()
+	Default                = $Flavor.Text.Foreground()
+	Error                  = $Flavor.Red.Foreground()
+	Keyword                = $Flavor.Mauve.Foreground()
+	Member                 = $Flavor.Rosewater.Foreground()
+	Number                 = $Flavor.Peach.Foreground()
+	Operator               = $Flavor.Sky.Foreground()
+	Parameter              = $Flavor.Pink.Foreground()
+	String                 = $Flavor.Green.Foreground()
+	Type                   = $Flavor.Yellow.Foreground()
+	Variable               = $Flavor.Lavender.Foreground()
 }
 
 $PSReadLineOptions = @{
@@ -23,8 +34,8 @@ $PSReadLineOptions = @{
   ShowToolTips = $true
 }
 
+Set-PSReadLineOption -Colors $Colors
 Set-PSReadLineOption @PSReadLineOptions
-Set-PSReadLineOption -Colors @{ InlinePrediction = "#86b567"}
 
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
