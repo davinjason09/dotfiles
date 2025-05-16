843b77c44bebe340e63e045ee7d7af05d88bd27f
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-16 22:31:39 +0700

    feat(nvim): snacks.nvim

--- home/dot_config/nvim/lua/plugins/init.lua
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -1,3 +1,47 @@
 require("config").init()
 
-return {}
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function(_, opts) require("snacks").setup(opts) end,
+    keys = {
+      { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { "<leader>dps", function() Snacks.profiler.scratch() end, desc = "Profiler Scratch Buffer" },
+      { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+      { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...) Snacks.debug.inspect(...) end
+          _G.bt = function() Snacks.debug.backtrace() end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+          -- Create some toggle mappings
+          -- stylua: ignore start
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }) :map("<leader>uc")
+          Snacks.toggle.diagnostics():map("<leader>ud")
+          Snacks.toggle.line_number():map("<leader>ul")
+          Snacks.toggle.treesitter():map("<leader>uT")
+          Snacks.toggle.dim():map("<leader>uD")
+          Snacks.toggle.animate():map("<leader>ua")
+          Snacks.toggle.indent():map("<leader>ug")
+          Snacks.toggle.scroll():map("<leader>uS")
+          Snacks.toggle.profiler():map("<leader>dpp")
+          Snacks.toggle.profiler_highlights():map("<leader>dph")
+          Snacks.toggle.zoom():map("<leader>wm"):map("<leader>uZ")
+          Snacks.toggle.zen():map("<leader>uz")
+          -- stylua: ignore end
+        end,
+      })
+    end,
+  },
+}
