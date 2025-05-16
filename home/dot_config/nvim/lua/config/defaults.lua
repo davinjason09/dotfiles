fd18bcc04147bd0e10a2e37c5f795a076640a317
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-16 23:00:22 +0700

    feat(nvim): default icons
    - kind: LSP kinds
    - diagnostics: diagnostic signs
    - modes = vim modes

--- home/dot_config/nvim/lua/config/defaults.lua
+++ home/dot_config/nvim/lua/config/defaults.lua
@@ -11,6 +11,67 @@ function M.filling_palette(palette) M.palette = palette end
 
 -- stylua: ignore start
 
+-- Default icons
+M.icons = {
+  kind = {
+    Array         = " ",
+    Boolean       = "󰨙 ",
+    Class         = " ",
+    Color         = "󱓻 ",
+    Control       = " ",
+    Constant      = "󰏿 ",
+    Constructor   = " ",
+    Copilot       = " ",
+    Enum          = " ",
+    EnumMember    = " ",
+    Event         = " ",
+    Field         = " ",
+    File          = " ",
+    Folder        = " ",
+    Function      = "󰡱 ",
+    Interface     = " ",
+    Key           = "󰌋 ",
+    Keyword       = " ",
+    Method        = " ",
+    Module        = " ",
+    Namespace     = "󰦮 ",
+    Null          = "󰟢 ",
+    Number        = "󰎠 ",
+    Object        = " ",
+    Operator      = " ",
+    Package       = " ",
+    Property      = " ",
+    Reference     = " ",
+    Snippet       = " ",
+    String        = " ",
+    Struct        = " ",
+    Text          = "󰉿 ",
+    TypeParameter = " ",
+    Unit          = " ",
+    Value         = "󰎠 ",
+    Variable      = " ",
+  },
+  diagnostics = {
+    ERROR = " ",
+    HINT  = " ",
+    INFO  = " ",
+    WARN  = " ",
+  },
+  modes = {
+    Normal   = "",
+    Insert   = "",
+    Visual   = "",
+    Command  = "",
+    Terminal = "",
+    Select   = "󰒉",
+    Replace  = "",
+    More     = "󰇙",
+    Confirm  = "",
+    Shell    = "",
+    Prompt   = "󰗧",
+  },
+}
+
 -- lazy.nvim default icons
 M.lazy_nvim_icons = {
   cmd        = "",
