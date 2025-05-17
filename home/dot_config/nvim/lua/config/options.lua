b4f5c0d4a57fdf8374b3f90fe851fc9b2b506ce5
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-17 23:54:34 +0700

    feat(nvim): options

---
+++ home/dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,114 @@
+-- stylua: ignore start
+-- ╾╼ Leader Key ╾────────────────────────────────────────────────────╼
+vim.g.mapleader      = " "
+vim.g.maplocalleader = "\\"
+
+-- ╾╼ General ╾───────────────────────────────────────────────────────╼
+vim.o.backup         = false       -- Don't store backup
+vim.o.confirm        = true        -- Ask for confirmation before overwriting files
+vim.o.laststatus     = 3           -- Always show status line
+vim.o.mouse          = "a"         -- Enable mouse support
+vim.o.mousemoveevent = true        -- Enable mouse move events
+vim.o.switchbuf      = "usetab"    -- Use already opened buffers when switching
+vim.o.writebackup    = false       -- Don't store backup files
+vim.o.timeoutlen     = 300         -- Time to wait for mapped sequence to complete
+vim.o.undofile       = true        -- Enable persistent undo
+vim.o.updatetime     = 250         -- Time to wait before writing swap file and triggering CursorHold
+
+vim.o.shada = "'100,<50,s10,:1000,/100,@100,h" -- Limit what is stored in ShaDa file
+
+vim.g.markdown_recommended_style = 0 -- Don't use recommended style for markdown
+vim.o.sessionoptions = table.concat( -- Options to save in session files
+  { "buffers", "curdir", "folds", "globals", "help", "skiprtp", "tabpages", "winsize" }, ","
+)
+
+-- ╾╼ UI ╾────────────────────────────────────────────────────────────╼
+vim.o.breakindent    = true        -- Indent wrapped lines to match the line start
+vim.o.cmdheight      = 0           -- Height of command line
+vim.o.conceallevel   = 2           -- Hide text until it is needed 
+vim.o.cursorline     = true        -- Highlight the line with the cursor
+vim.o.cursorlineopt  = "both"      -- Highlight the line with the cursor
+vim.o.linebreak      = true        -- Wrap long lines
+vim.o.number         = true        -- Show line numbers
+vim.o.relativenumber = true        -- Show relative line numbers
+vim.o.ruler          = false       -- Don't show cursor position
+vim.o.shortmess      = 'FOSWaco'   -- Disable certain messages from |ins-completion-menu|
+vim.o.showmode       = false       -- Don't show mode in command line
+vim.o.signcolumn     = 'yes'       -- Always show signcolumn or it would frequently shift
+vim.o.splitbelow     = true        -- Horizontal splits will be below
+vim.o.splitright     = true        -- Vertical splits will be to the right
+vim.o.splitkeep      = "screen"    -- Keep the text on the same screen line 
+vim.o.winminwidth    = 5           -- Minimum window width
+vim.o.wrap           = false       -- Display long lines as just one line
+
+vim.o.fillchars = table.concat(    -- Special UI symbols
+  { "diff:/", "eob: ", "fold: ", "foldsep: ", "foldopen:", "foldclose:" }, ","
+)
+vim.o.listchars = table.concat(    -- Special text symbols
+  { "eol:↩", "extends:…", "nbsp:␣", "precedes:…", "tab:»·", "trail:·" }, ","
+)
+vim.o.breakindentopt = "list:-1"   -- Add padding for list when `wrap` is enabled
+vim.o.cursorlineopt  = "both"      -- Highlight text line and number where the cursor is
+vim.o.termguicolors  = true        -- Enable 24-bit RGB colors in the TUI
+vim.o.winborder      = "none"      -- Use no border for windows (at least until all plugins are updated to 0.11)
+
+vim.o.guicursor      = "n-v-sm:block,i-c-ci-ve-t:ver25,r-cr-o:hor20"    -- Change terminal and command mode cursor to bar
+vim.o.statuscolumn   = [[%!v:lua.require('snacks.statuscolumn').get()]] -- Custom status column
+
+-- ╾╼ Editing ╾───────────────────────────────────────────────────────╼
+vim.o.autoindent    = true         -- Use auto indent
+vim.o.expandtab     = true         -- Convert tabs to spaces
+vim.o.formatoptions = "rqnl1j"     -- Improve comment editing
+vim.o.ignorecase    = true         -- Ignore case when searching
+vim.o.incsearch     = true         -- Show search matches as you type
+vim.o.shiftwidth    = 2            -- Number of spaces for indentation
+vim.o.shiftround    = true         -- Round indent to multiple of 'shiftwidth'
+vim.o.softtabstop   = 2            -- Number of spaces for a tab when editing
+vim.o.smartcase     = true         -- Don't ignore case when searching with uppercase letters
+vim.o.smartindent   = true         -- Make indenting smarter
+vim.o.tabstop       = 2            -- Number of spaces for a tab
+vim.o.virtualedit   = "block"      -- Allow going past the end of line in V-BLOCK mode
+
+vim.o.iskeyword     = '@,48-57,_,192-255,-'              -- Treat dash separated words as a word text object
+vim.o.scrolloff     = math.floor(0.3 * vim.o.lines)   -- Keep 30% of the screen height above and below the cursor
+vim.o.sidescrolloff = math.floor(0.3 * vim.o.columns) -- Keep 30% of the screen width to the left and right of the cursor
+
+-- ╾╼ Folds ╾─────────────────────────────────────────────────────────╼
+vim.o.foldcolumn = "auto"          -- Fold column width
+vim.o.foldlevel  = 99              -- Display all folds
+vim.o.foldmethod = "expr"          -- Use expression for folding
+vim.o.foldtext   = ""              -- Use underlying text with its highlighting
+vim.o.foldenable = false           -- Disable folding by default
+vim.g.markdown_filding = 1         -- Use folding by heading in markdown files
+
+vim.o.foldexpr   = "v:lua.require('utils').foldexpr()" -- Custom fold expression
+
+-- ╾╼ Spelling ╾──────────────────────────────────────────────────────╼
+vim.o.spelllang    = "en"          -- Default spelling dictionary
+vim.o.spelloptions = "camel"       -- Treat camel case words as separate words
+
+-- ╾╼ Title ╾─────────────────────────────────────────────────────────╼
+function GetFtIcon()
+  local MiniIcons = require("mini.icons")
+  local filename = vim.fn.expand("%:t")
+  local icon, _, is_default = MiniIcons.get("file", filename)
+  return is_default and "" or icon .. " "
+end
+
+vim.o.title = true
+vim.o.titlestring = '%{v:lua.GetFtIcon()}%{expand("%:t")} -  %{fnamemodify(getcwd(), ":t")}'
+
+-- ╾╼ Clipboard ╾─────────────────────────────────────────────────────╼
+vim.o.clipboard = "unnamedplus" -- Use system clipboard for all operations
+vim.g.clipboard = {             -- WSL clipboard (win32yank) 
+  name = "wsl-clipboard",
+  copy = {
+    ["+"] = { "wslyank", "-i", "--crlf" },
+    ["*"] = { "wslyank", "-i", "--crlf" },
+  },
+  paste = {
+    ["+"] = { "wslyank", "-o", "--lf" },
+    ["*"] = { "wslyank", "-o", "--lf" },
+  },
+  cache_enabled = 0,
+}
