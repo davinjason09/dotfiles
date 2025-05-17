34c93d34824a460d8454a3326ced6d1c20d33be6
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-18 00:34:49 +0700

    fix(nvim): fix picker keymaps
    The keymap wasn't registering, and after some digging, it turns out that
    the indentation of the key was wrong, so the it ended up inside the opts
    table.

--- home/dot_config/nvim/lua/plugins/picker.lua
+++ home/dot_config/nvim/lua/plugins/picker.lua
@@ -32,12 +32,8 @@ return {
             list = { keys = { ["d"] = "bufdelete" } },
           },
         },
-        diagnostics = {
-          layout = "dropdown",
-        },
-        diagnostics_buffer = {
-          layout = "dropdown",
-        },
+        diagnostics = { layout = "dropdown" },
+        diagnostics_buffer = { layout = "dropdown" },
         keymaps = {
           plugs = true,
           layout = {
@@ -45,9 +41,7 @@ return {
             layout = { width = 0.8, height = 0.6 },
           },
         },
-        undo = {
-          layout = "dropdown",
-        },
+        undo = { layout = "dropdown" },
       },
       win = {
         input = {
@@ -70,44 +64,44 @@ return {
         kinds = Defaults.icons.kind,
       },
     },
-    -- stylua: ignore
-    keys = {
-      -- Top Pickers
-      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-      { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-      { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
-      -- Find
-      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "[F]ind [B]uffers" },
-      { "<leader>fg", function() Snacks.picker.grep() end, desc = "[F]ind by [G]rep" },
-      { "<leader>fw", function() Snacks.picker.grep_word() end, desc = "[F]ind [W]ord", mode = { "n", "x" } },
-      { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "[F]ind [C]onfig File" },
-      { "<leader>ff", function() Snacks.picker.files() end, desc = "[F]ind [F]iles" },
-      { "<leader>fp", function() Snacks.picker.projects() end, desc = "[F]ind [P]rojects" },
-      { "<leader>fr", function() Snacks.picker.recent() end, desc = "[F]ind [R]ecent" },
-      { "<leader>fl", function() Snacks.picker.lines() end, desc = "[F]ind [L]ines" },
-      { "<leader>fh", function() Snacks.picker.help() end, desc = "[F]ind [H]elp" },
-      { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "[F]ind [K]eymaps" },
-      -- Search
-      { '<leader>s"', function() Snacks.picker.registers() end, desc = "[S]earch Registers [\"]" },
-      { '<leader>s/', function() Snacks.picker.search_history() end, desc = "[S]earch History [/]" },
-      { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "[S]earch [A]utocmds" },
-      { "<leader>sc", function() Snacks.picker.command_history() end, desc = "[S]earch [C]ommand History" },
-      { "<leader>sC", function() Snacks.picker.commands() end, desc = "[S]earch [C]ommands" },
-      { "<leader>sh", function() Snacks.picker.highlights() end, desc = "[S]earch [H]ighlights" },
-      { "<leader>si", function() Snacks.picker.icons() end, desc = "[S]earch [I]cons" },
-      { "<leader>sj", function() Snacks.picker.jumps() end, desc = "[S]earch [J]umps" },
-      { "<leader>sl", function() Snacks.picker.loclist() end, desc = "[S]earch [L]ocation List" },
-      { "<leader>sm", function() Snacks.picker.marks() end, desc = "[S]earch [M]arks" },
-      { "<leader>sM", function() Snacks.picker.man() end, desc = "[S]earch [M]an Pages" },
-      { "<leader>sp", function() Snacks.picker.lazy() end, desc = "[S]earch [P]lugin Spec" },
-      { "<leader>sq", function() Snacks.picker.qflist() end, desc = "[S]earch [Q]uickfix List" },
-      { "<leader>sR", function() Snacks.picker.resume() end, desc = "[S]earch: [R]esume Search" },
-      { "<leader>su", function() Snacks.picker.undo() end, desc = "[S]earch [U]ndo History" },
-      -- Diagnostics
-      { "<leader>xd", function() Snacks.picker.diagnostics() end, desc = "Global Diagnostics" },
-      { "<leader>xD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-      -- Misc
-      { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
-    },
+  },
+  -- stylua: ignore
+  keys = {
+    -- Top Pickers
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+    -- Find
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "[F]ind [B]uffers" },
+    { "<leader>fg", function() Snacks.picker.grep() end, desc = "[F]ind by [G]rep" },
+    { "<leader>fw", function() Snacks.picker.grep_word() end, desc = "[F]ind [W]ord", mode = { "n", "x" } },
+    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "[F]ind [C]onfig File" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "[F]ind [F]iles" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "[F]ind [P]rojects" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "[F]ind [R]ecent" },
+    { "<leader>fl", function() Snacks.picker.lines() end, desc = "[F]ind [L]ines" },
+    { "<leader>fh", function() Snacks.picker.help() end, desc = "[F]ind [H]elp" },
+    { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "[F]ind [K]eymaps" },
+    -- Search
+    { '<leader>s"', function() Snacks.picker.registers() end, desc = "[S]earch Registers [\"]" },
+    { '<leader>s/', function() Snacks.picker.search_history() end, desc = "[S]earch History [/]" },
+    { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "[S]earch [A]utocmds" },
+    { "<leader>sc", function() Snacks.picker.command_history() end, desc = "[S]earch [C]ommand History" },
+    { "<leader>sC", function() Snacks.picker.commands() end, desc = "[S]earch [C]ommands" },
+    { "<leader>sh", function() Snacks.picker.highlights() end, desc = "[S]earch [H]ighlights" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "[S]earch [I]cons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "[S]earch [J]umps" },
+    { "<leader>sl", function() Snacks.picker.loclist() end, desc = "[S]earch [L]ocation List" },
+    { "<leader>sm", function() Snacks.picker.marks() end, desc = "[S]earch [M]arks" },
+    { "<leader>sM", function() Snacks.picker.man() end, desc = "[S]earch [M]an Pages" },
+    { "<leader>sp", function() Snacks.picker.lazy() end, desc = "[S]earch [P]lugin Spec" },
+    { "<leader>sq", function() Snacks.picker.qflist() end, desc = "[S]earch [Q]uickfix List" },
+    { "<leader>sR", function() Snacks.picker.resume() end, desc = "[S]earch: [R]esume Search" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "[S]earch [U]ndo History" },
+    -- Diagnostics
+    { "<leader>xd", function() Snacks.picker.diagnostics() end, desc = "Global Diagnostics" },
+    { "<leader>xD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+    -- Misc
+    { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
   },
 }
