95e7bf4d141754204a2d6af13d1b1ae7535ed244
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-22 14:45:31 +0700

    feat(nvim): blink.cmp keymaps and sources

--- home/dot_config/nvim/lua/plugins/completion.lua
+++ home/dot_config/nvim/lua/plugins/completion.lua
@@ -32,6 +32,75 @@ return {
     },
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lsp = {
+          timeout_ms = 500,
+          opts = { tailwind_color_icon = Defaults.icons.kind.Color },
+        },
+        buffer = { max_items = 5 },
+        cmdline = {
+          min_keyword_length = function(ctx)
+            if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then return 3 end
+            return 0
+          end,
+        },
+      },
+    },
+    keymap = {
+      preset = "none",
+      ["<A-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<BS>"] = {
+        function(cmp)
+          if cmp.is_menu_visible() then cmp.cancel() end
+
+          local bs = vim.api.nvim_replace_termcodes("<BS>", true, false, true)
+          vim.defer_fn(function() vim.fn.feedkeys(bs, "n") end, 1)
+        end,
+      },
+
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    },
+    cmdline = {
+      completion = {
+        list = { selection = { auto_insert = false } },
+        menu = {
+          auto_show = function() return vim.fn.getcmdtype() == ":" end,
+          draw = {
+            columns = { { "kind_icon" }, { "label" }, { "kind" } },
+          },
+        },
+      },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+
+        if type == "/" or type == "?" then return { "buffer" } end
+
+        -- HACK:
+        -- Disable cmdline completion for shell command (!) because it makes neovim hangs.
+        -- Only allow completion if ("!%w+$") is not found in the line
+        if type == ":" and not vim.fn.getcmdline():match("!%w+$") then return { "cmdline" } end
+
+        return {}
+      end,
+      keymap = {
+        preset = "none",
+        ["<A-Space>"] = { "show", "fallback" },
+        ["<Tab>"] = { "show_and_insert", "accept" },
+        ["<C-Right>"] = { "accept", "fallback" },
+        ["<CR>"] = { "select_and_accept", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+      },
     },
   },
 }
