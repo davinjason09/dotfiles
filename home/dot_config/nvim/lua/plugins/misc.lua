9ebfc9debc7201a5e130f17adeab9b0d2c9a1644
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-30 16:35:11 +0700

    feat(nvim): miscellaneous plugins
    - comment-box.nvim (Comment styling)
    - cord.nvim (Discord presence)

---
+++ home/dot_config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "LudoPinelli/comment-box.nvim",
+    event = "InsertEnter",
+    opts = {
+      doc_width = 80,
+      box_width = 60,
+      line_width = 70,
+    },
+    -- stylua: ignore
+    keys = {
+      { "gcb", ":CBccbox<CR>",    desc = "[C]omment - [B]ox",        mode = { "n", "v" } },
+      { "gct", ":CBllline12<CR>", desc = "[C]omment - [T]itle Line", mode = { "n", "v" } },
+      { "gcl", ":CBlline<CR>",    desc = "[C]omment - [L]ine",       mode = { "n", "v" } },
+      { "gcm", ":CBllbox14<CR>",  desc = "[C]omment - [M]arked",     mode = { "n", "v" } },
+      { "gcd", ":CBd<CR>",        desc = "[C]omment - [D]elete",     mode = { "n", "v" } },
+    },
+  },
+  {
+    "vyfor/cord.nvim",
+    build = ":Cord update",
+    event = "VeryLazy",
+    opts = {
+      editor = { tooltip = "I use Neovim btw" },
+      log_level = vim.log.levels.OFF,
+      display = {
+        theme = "atom",
+        flavor = "accent",
+      },
+      hooks = {
+        ---@diagnostic disable-next-line: unused-vararg
+        ready = function(...) vim.notify("  Connected to Discord!") end,
+      },
+    },
+  },
+}
