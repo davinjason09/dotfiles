13f48e755dac68c8305a5b7f486408b93b1740c6
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-05-30 15:48:11 +0700

    feat(nvim): more autocommands

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,13 @@
 local function augroup(name) return vim.api.nvim_create_augroup(name, { clear = true }) end
 
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("CheckTime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then vim.cmd("checktime") end
+  end,
+  desc = "Check if we need to reload the file when it changed",
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = augroup("HighlightYank"),
   callback = function()
@@ -7,6 +15,43 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     Utils.restore_cursor()
   end,
   desc = "Highlight on yank and preserve cursor position",
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup("ResizeSplits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+  desc = "Resize splits if window got resized",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("CloseWithQ"),
+  pattern = {
+    "checkhealth",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "query",
+    "qf",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
+  end,
+  desc = "Close certain buffer with <q>",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
