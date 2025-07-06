73ad231437b82dfc44d33e4da0813be83d4c84a9
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-07-06 20:17:30 +0700

    feat(clangd): add clangd config, and remove the settings from the nvim's clangd.lua

--- home/dot_config/nvim/lsp/clangd.lua
+++ home/dot_config/nvim/lsp/clangd.lua
@@ -8,12 +8,16 @@ return {
     "--completion-style=detailed",
     "--function-arg-placeholders",
     "--fallback-style=llvm",
+    "--all-scopes-completion",
+    "--suggest-missing-includes",
+    "--offset-encoding=utf-16",
   },
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
   root_markers = {
     ".clangd",
     ".clang-tidy",
     ".clang-format",
+    ".git",
     "compile_commands.json",
     "compile_flags.txt",
     "configure.ac",
@@ -21,11 +25,13 @@ return {
     "build.ninja",
     "meson.build",
   },
-  init_options = {
-    clangdFileStatus = true,
-    usePlaceholders = true,
-    completeUnimported = true,
-    semanticHighlighting = true,
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { "utf-8", "utf-16" },
   },
   -- stylua: ignore
   on_attach = function(_, bufnr)
@@ -36,19 +42,4 @@ return {
     map("n", "<leader>cm", "<CMD>ClangdMemoryUsage<CR>", { buffer = bufnr, desc = "[C]ode: Show [M]emory Usage" })
     map({ "n", "v" }, "<leader>cA", "<cmd>ClangAST<CR>", { buffer = bufnr, desc = "[C]ode: Show [A]ST" })
   end,
-  settings = {
-    clangd = {
-      semanticHighlighting = true,
-      single_file_support = false,
-    },
-  },
-  single_file_support = true,
-  capabilities = {
-    textDocument = {
-      completion = {
-        editsNearCursor = true,
-      },
-    },
-    offsetEncoding = { "utf-8", "utf-16" },
-  },
 }
