91d0841f3015af527e8a4ad7abf143da20c8aaab
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-07-31 20:42:24 +0700

    feat(nvim): simplify cursor restore and center the screen after restoring it

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -28,18 +28,20 @@ vim.api.nvim_create_autocmd("VimResized", {
 })
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup("last_loc"),
+  group = augroup("LastCursorPosition"),
   callback = function(event)
     local exclude = { "gitcommit" }
     local buf = event.buf
 
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then return end
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then return end
 
-    vim.b[buf].last_loc = true
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
 
-    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      vim.cmd("normal! zz")
+    end
   end,
   desc = "Jump to last location in file",
 })
