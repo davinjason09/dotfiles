7add68c191880d7d18cc86cec183c484b8617cb7
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-08-17 13:42:59 +0700

    feat(nvim): use the new terminal module

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -263,10 +263,17 @@ map("n", "<C-k>", "<C-w>k", { remap = true, desc = "Go to Upper Window" })
 map("n", "<C-l>", "<C-w>l", { remap = true, desc = "Go to Right Window" })
 
 -- ╭─────────────────────────────────────────────────────────╮
+-- │                        Terminal                         │
+-- ╰─────────────────────────────────────────────────────────╯
+
+local Terminal = Utils.lazy_require("custom.terminal")
+map({ "n", "i", "x" }, "<C-`>", Terminal.toggle, { desc = "Toggle Terminal" })
+
+-- ╭─────────────────────────────────────────────────────────╮
 -- │                           Git                           │
 -- ╰─────────────────────────────────────────────────────────╯
 
-map("n", "<leader>gg", function() Utils.terminal.open("lazygit") end, { desc = "[G]it: Lazy[G]it" })
+map("n", "<leader>gg", function() Terminal.open("lazygit") end, { desc = "[G]it: Lazy[G]it" })
 map({ "n", "x" }, "<leader>gB", function() Snacks.gitbrowse() end, { desc = "[G]it: [B]rowse (open)" })
 map({ "n", "x" }, "<leader>gY", function()
   ---@diagnostic disable-next-line: missing-fields
