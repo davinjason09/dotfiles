21bef18ebc0f45d0a1f4b24ebd6edcecaef11dc9
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-08-18 21:51:37 +0700

    feat(glazewm,chezmoiscripts): added scripts
    - Add scripts that will be run when glazewm start and reload
    - Change how the chezmoiscripts handle copying file to the Windows side

---
+++ home/dot_config/glazewm/scripts/disablewintoopenmenu.ahk
@@ -0,0 +1,10 @@
+#SingleInstance Force
+#Requires AutoHotkey v2.0
+
+~LWin:: { 
+  Send "{Blind}{vkE8}"
+}
+
+^+Esc:: {
+  ExitApp
+}
---
+++ home/dot_config/glazewm/scripts/start.ps1
@@ -0,0 +1,21 @@
+function Has-Started {
+  param (
+    [string]$name
+  )
+  $started = Get-Process | Where-Object { $_.Name -eq $name }
+
+  return $started -ne $null
+}
+
+
+Start-ScheduledTask -TaskName "Tacky-Borders"
+start $HOME/.glzr/scripts/disablewintoopenmenu.ahk
+start Everything --startup
+
+if (-not (Has-Started "Flow.Launcher")) {
+  start FlowLauncher
+}
+
+if (-not (Has-Started "kanata-tray")) {
+  start kanata-tray
+}
