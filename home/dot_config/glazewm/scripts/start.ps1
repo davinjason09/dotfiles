1b263253f48dd772b0ed66cc4ebf0bfb3130432e
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-10-07 16:17:58 +0700

    feat(glazewm): start yasb after glazemw starts and reorder startup order

--- home/dot_config/glazewm/scripts/start.ps1
+++ home/dot_config/glazewm/scripts/start.ps1
@@ -8,12 +8,17 @@ function Has-Started {
 }
 
 Start-ScheduledTask -TaskName "Tacky-Borders"
-start $HOME/.glzr/scripts/disablewintoopenmenu.ahk
-start Everything --startup
+
+if (-not (Has-Started "yasb")) {
+  yasbc start
+}
 
 if (-not (Has-Started "Flow.Launcher")) {
   start FlowLauncher
 }
+
+start $HOME/.glzr/scripts/disablewintoopenmenu.ahk
+start Everything --startup
 
 if (-not (Has-Started "kanata-tray")) {
   start kanata-tray
