f72ef2becaf4d010037e5b3da0db175e68ece1a7
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-10-24 23:36:14 +0700

    feat(wezterm): keybinds

---
+++ home/dot_config/wezterm/config/keymaps.lua
@@ -0,0 +1,71 @@
+local wez = require("wezterm") ---@type Wezterm
+local act = wez.action
+
+local mod = {
+  CTRL = "CTRL",
+  CSHIFT = "CTRL|SHIFT",
+  LEADER = "LEADER",
+}
+
+-- stylua: ignore
+local keys = {
+  -- Activate Tabs
+  { key = "Tab", mods = mod.CTRL,   action = act.ActivateTabRelative(1) },
+  { key = "Tab", mods = mod.CSHIFT, action = act.ActivateTabRelative(-1) },
+  { key = "1",   mods = mod.CTRL,   action = act.ActivateTab(0) },
+  { key = "2",   mods = mod.CTRL,   action = act.ActivateTab(1) },
+  { key = "3",   mods = mod.CTRL,   action = act.ActivateTab(2) },
+  { key = "4",   mods = mod.CTRL,   action = act.ActivateTab(3) },
+  { key = "5",   mods = mod.CTRL,   action = act.ActivateTab(4) },
+
+  -- Split, Resize, Rotate, and Activate Panes
+  { key = "-",          mods = mod.LEADER, action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "\\",         mods = mod.LEADER, action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "h",          mods = mod.LEADER, action = act.ActivatePaneDirection("Left") },
+  { key = "h",          mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Left", 1 }) },
+  { key = "j",          mods = mod.LEADER, action = act.ActivatePaneDirection("Down") },
+  { key = "j",          mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Down", 1 }) },
+  { key = "k",          mods = mod.LEADER, action = act.ActivatePaneDirection("Up") },
+  { key = "k",          mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Up", 1 }) },
+  { key = "l",          mods = mod.LEADER, action = act.ActivatePaneDirection("Right") },
+  { key = "l",          mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Right", 1 }) },
+  { key = "LeftArrow",  mods = mod.LEADER, action = act.ActivatePaneDirection("Left") },
+  { key = "LeftArrow",  mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Left", 1 }) },
+  { key = "DownArrow",  mods = mod.LEADER, action = act.ActivatePaneDirection("Down") },
+  { key = "DownArrow",  mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Down", 1 }) },
+  { key = "UpArrow",    mods = mod.LEADER, action = act.ActivatePaneDirection("Up") },
+  { key = "UpArrow",    mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Up", 1 }) },
+  { key = "RightArrow", mods = mod.LEADER, action = act.ActivatePaneDirection("Right") },
+  { key = "RightArrow", mods = mod.CSHIFT, action = act.AdjustPaneSize({ "Right", 1 }) },
+  { key = "0",          mods = mod.LEADER, action = act.PaneSelect({ mode = "SwapWithActive", alphabet = "1234567890" }) },
+  { key = "r",          mods = mod.LEADER, action = act.RotatePanes("Clockwise") },
+  { key = "R",          mods = mod.LEADER, action = act.RotatePanes("CounterClockwise") },
+
+  -- Adjust Font Size
+  { key = "=", mods = mod.CTRL, action = act.IncreaseFontSize },
+  { key = "-", mods = mod.CTRL, action = act.DecreaseFontSize },
+  { key = "0", mods = mod.CTRL, action = act.ResetFontSize },
+
+  -- Copy Mode
+  { key = "X", mods = mod.CSHIFT, action = act.ActivateCopyMode },
+  { key = "C", mods = mod.CSHIFT, action = act.CopyTo("Clipboard") },
+  { key = "V", mods = mod.CSHIFT, action = act.PasteFrom("Clipboard") },
+
+  -- Other
+  { key = "F5", mods = mod.CTRL,   action = act.ReloadConfiguration },
+  { key = "F",  mods = mod.CSHIFT, action = act.Search("CurrentSelectionOrEmptyString") },
+  { key = "k",  mods = mod.LEADER, action = act.ClearScrollback("ScrollbackOnly") },
+  { key = "l",  mods = mod.LEADER, action = act.ShowDebugOverlay },
+  { key = "N",  mods = mod.CSHIFT, action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "P",  mods = mod.CSHIFT, action = act.ActivateCommandPalette },
+  { key = "q",  mods = mod.LEADER, action = act.CloseCurrentPane({ confirm = false }) },
+  { key = "T",  mods = mod.CSHIFT, action = act.ShowLauncher },
+  { key = "U",  mods = mod.CSHIFT, action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }) },
+  { key = "/",  mods = mod.LEADER, action = act.Search({ CaseInSensitiveString = "" }) },
+}
+
+return {
+  disable_default_key_bindings = true,
+  leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1500 },
+  keys = keys,
+}
--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -4,5 +4,6 @@ local config = require("config")
   :append(require("config.appearance"))
   :append(require("config.domains"))
   :append(require("config.general"))
+  :append(require("config.keymaps"))
 
 return config
