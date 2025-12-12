dfb03b8d34d304ea32646119f82b3726de14c6ec
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-12-12 10:46:49 +0700

    fix(nvim): fix tabline sometimes not updating
    - For some reason the BufAdd event is not updating the _buflist_cache,
    so we add BufRead just in case
    - Wrap the buf_map calculation using vim.schedule

--- home/dot_config/nvim/lua/custom/lines/init.lua
+++ home/dot_config/nvim/lua/custom/lines/init.lua
@@ -21,25 +21,23 @@ M.setup = function()
 
   Comp.AI.setup()
 
-  vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete" }, {
-    callback = function()
-      vim.schedule(function()
-        local buffers = U.get_bufs()
-        for i, v in ipairs(buffers) do
-          M._buflist_cache[i] = v
-        end
+  vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete", "BufRead" }, {
+    callback = vim.schedule_wrap(function()
+      local buffers = U.get_bufs()
+      for i, v in ipairs(buffers) do
+        M._buflist_cache[i] = v
+      end
 
-        for i = #buffers + 1, #M._buflist_cache do
-          M._buflist_cache[i] = nil
-        end
+      for i = #buffers + 1, #M._buflist_cache do
+        M._buflist_cache[i] = nil
+      end
 
-        if #M._buflist_cache > 1 then
-          vim.o.showtabline = 2
-        elseif vim.o.showtabline ~= 1 then -- otherwise it breaks startup screen
-          vim.o.showtabline = 1
-        end
-      end)
-    end,
+      if #M._buflist_cache > 1 then
+        vim.o.showtabline = 2
+      elseif vim.o.showtabline ~= 1 then -- otherwise it breaks startup screen
+        vim.o.showtabline = 1
+      end
+    end),
   })
 
   -- HACK: update the showtabline to make sure the tabline macro component gets updated
@@ -53,53 +51,50 @@ M.setup = function()
     end,
   })
 
-  vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete", "TermOpen" }, {
-    callback = function()
-      local seen = {}
-      local items = vim.tbl_map(function(bufnr)
-        local path = vim.api.nvim_buf_get_name(bufnr)
-        local parts = vim.split(path, "/")
-        return {
-          path = path,
-          parts = parts,
-          depth = 1,
-          name = parts[#parts],
-        }
-      end, U.get_bufs())
+  vim.api.nvim_create_autocmd(
+    { "VimEnter", "UIEnter", "BufAdd", "BufRead", "BufDelete", "TermOpen" },
+    {
+      callback = vim.schedule_wrap(function()
+        local seen = {}
+        local items = vim.tbl_map(function(bufnr)
+          local path = vim.api.nvim_buf_get_name(bufnr)
+          local parts = vim.split(path, "/")
+          return { path = path, parts = parts, depth = 1, name = parts[#parts] }
+        end, U.get_bufs())
 
-      vim.iter(items):map(function(item) seen[item.name] = (seen[item.name] or 0) + 1 end)
+        vim.iter(items):map(function(item) seen[item.name] = (seen[item.name] or 0) + 1 end)
 
-      while true do
-        local changes_made = false
+        while true do
+          local changes_made = false
 
-        vim.iter(items):map(function(item)
-          if seen[item.name] > 1 and item.depth < #item.parts then
-            item.depth = item.depth + 1
-            item.name = table.concat(item.parts, "/", #item.parts - item.depth + 1)
-            seen[item.name] = (seen[item.name] or 0) + 1
-            changes_made = true
-          end
+          vim.iter(items):map(function(item)
+            if seen[item.name] > 1 and item.depth < #item.parts then
+              item.depth = item.depth + 1
+              item.name = table.concat(item.parts, "/", #item.parts - item.depth + 1)
+              seen[item.name] = (seen[item.name] or 0) + 1
+              changes_made = true
+            end
+          end)
+
+          if not changes_made then break end
+        end
+
+        local res = {}
+        vim.iter(items):map(function(i)
+          local parent_dir = table.concat(i.parts, "/", #i.parts - i.depth + 1, #i.parts - 1)
+          local tail = i.parts[#i.parts]
+
+          res[i.path] = {
+            parent = parent_dir .. (parent_dir ~= "" and "/" or ""),
+            tail = tail ~= "" and tail or "[No Name]",
+          }
         end)
 
-        if not changes_made then break end
-      end
-
-      local res = {}
-      vim.iter(items):map(function(i)
-        local parent_dir = table.concat(i.parts, "/", #i.parts - i.depth + 1, #i.parts - 1)
-        local tail = i.parts[#i.parts]
-
-        res[i.path] = {
-          parent = parent_dir .. (parent_dir ~= "" and "/" or ""),
-          tail = tail ~= "" and tail or "[No Name]",
-        }
-      end)
-
-      ---@diagnostic disable-next-line: inject-field
-      require("heirline").tabline.buf_map = res
-    end,
-  })
-  -- Tab.setup()
+        ---@diagnostic disable-next-line: inject-field
+        require("heirline").tabline.buf_map = res
+      end),
+    }
+  )
 end
 
 return M
