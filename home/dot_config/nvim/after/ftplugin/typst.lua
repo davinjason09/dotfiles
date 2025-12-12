5f9e284745bb2411df150ccd283619051241fbd9
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2025-12-12 10:26:50 +0700

    feat(nvim): add keymaps and autocommand to typst ftplugin
    - Automatically set tinymist's main file if the current file name
    contains main.typ and it is located in the CWD or one folder deeper
    - Add keymaps to surround word / selection (bold, italic, strikethrough,
    underline)
    - Fix command name (TinymistExportPdf)

--- home/dot_config/nvim/after/ftplugin/typst.lua
+++ home/dot_config/nvim/after/ftplugin/typst.lua
@@ -14,10 +14,56 @@ local function export_file()
   end)
 end
 
+---@param word string
+local function surround(word)
+  local start = vim.fn.mode() == "v" and "sa" or "saiw"
+  local rest = word
+  if vim.startswith(word, "#") and #word > 1 then rest = "]F[i" .. word .. "<ESC>w" end
+
+  vim.fn.feedkeys(Snacks.util.keycode(start .. rest))
+end
+
 local map = vim.keymap.set
--- stylua: ignore
-map("n", "<leader>ce", "<CMD>LspTinymistExportPdf<CR>", { desc = "[C]ode: [E]xport PDF", buffer = true })
+-- stylua: ignore start
+map("n", "<leader>ce", "<CMD>TinymistExportPdf<CR>", { desc = "[C]ode: [E]xport PDF", buffer = true })
 map("n", "<leader>cE", export_file, { desc = "[C]ode: [E]xport Picker", buffer = true })
+map({ "n", "x" }, "<A-b>", function() surround("*") end,          { desc = "Surround with *bold*", buffer = true })
+map({ "n", "x" }, "<A-i>", function() surround("_") end,          { desc = "Surround with _italic_", buffer = true })
+map({ "n", "x" }, "<A-u>", function() surround("#underline") end, { desc = "Surround with underline", buffer = true })
+map({ "n", "x" }, "<A-s>", function() surround("#strike") end,    { desc = "Surround with strikethrough", buffer = true })
+-- stylua: ignore end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("TypstPinMain", { clear = true }),
+  callback = function(ev)
+    local allowed_main_name = { "main.typ" }
+
+    local buf_name = vim.fn.fnamemodify(ev.file, ":t")
+    local buf_path = vim.fn.fnamemodify(ev.file, ":p:h")
+    local cwd = vim.fn.getcwd()
+    local client = vim.lsp.get_clients({ name = "tinymist" })[1]
+
+    if not client then return end
+    if not vim.tbl_contains(allowed_main_name, buf_name) then return end
+
+    if vim.g.typst_main_file ~= nil then
+      local main_buf_path = vim.fn.fnamemodify(vim.g.typst_main_file, ":p:h")
+      if main_buf_path == cwd or buf_path ~= cwd then return end
+    end
+
+    vim.g.typst_main_file = ev.file
+    client:exec_cmd({
+      title = "Pin main file",
+      command = "tinymist.pinMain",
+      arguments = { ev.file },
+    })
+    vim.notify(
+      ("Pinned %s as typst main file"):format(vim.fn.fnamemodify(ev.file, ":.")),
+      vim.log.levels.INFO,
+      { title = "tinymist" }
+    )
+  end,
+})
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.typ",
