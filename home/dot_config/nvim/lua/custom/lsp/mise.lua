93ff74c08aaa61b3d3a4eba36d5f330d748a2590
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-01-11 10:54:23 +0700

    feat(nvim): load LSP path with mise
    This ensures we only add the path when using nvim

---
+++ home/dot_config/nvim/lua/custom/lsp/mise.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+local opts = {
+  cmd = "mise",
+  args = "env --json",
+  initial_path = vim.env.PATH,
+}
+
+local function get_data()
+  local full_command = opts.cmd .. " " .. opts.args
+  local env_sh = vim.fn.system(full_command)
+
+  if env_sh:find("^mise") ~= nil then
+    return vim.notify(env_sh:match("^[^\n]*"), vim.log.levels.ERROR)
+  end
+
+  local ok, data = pcall(vim.json.decode, env_sh)
+  if not ok or data == nil then
+    return vim.notify('Invalid json returned by "' .. full_command .. '"', vim.log.levels.ERROR)
+  end
+
+  return data
+end
+
+local function load_env(data)
+  for var_name, var_value in pairs(data) do
+    vim.env[var_name] = var_value
+  end
+end
+
+M.setup = function()
+  local data = get_data()
+  if data == nil then return end
+
+  vim.env.PATH = opts.initial_path
+  load_env(data)
+end
+
+return M
