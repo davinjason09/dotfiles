acd57f060a41030c3f3ebfd6d3b53596c1785189
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-01-12 17:15:32 +0700

    feat(nvim): astro-ls, tsgo, tailwindcss-ls setup
    - Config for tsgo, astro-ls, and tailwindcss-ls
    - Treesitter queries for those filetypes

---
+++ home/dot_config/nvim/lsp/tsgo.lua
@@ -0,0 +1,24 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "tsgo", "--lsp", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  root_dir = function(bufnr, on_dir)
+    local root_markers = {
+      { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" },
+      { ".git" },
+    }
+
+    if vim.fs.root(bufnr, { "deno.json", "deno.jsonc", "deno.lock" }) then return end
+
+    local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+    on_dir(project_root)
+  end,
+}
