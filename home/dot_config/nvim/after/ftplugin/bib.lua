5754a4ba32f2b18439325f4da0e943213e2490e1
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-02-19 21:24:14 +0700

    feat(nvim): check for bibget executable before getting the bibtex

--- home/dot_config/nvim/after/ftplugin/bib.lua
+++ home/dot_config/nvim/after/ftplugin/bib.lua
@@ -1,4 +1,8 @@
 vim.api.nvim_buf_create_user_command(0, "GetBib", function(ctx)
+  if not vim.fn.executable("bibget") then
+    return vim.notify("`bibget` binary is not found in PATH", vim.log.levels.ERROR)
+  end
+
   local row = vim.api.nvim_win_get_cursor(0)[1]
   local buf = vim.api.nvim_get_current_buf()
 
