7dc2d18889c1a6d4c21118051a469acba83afc93
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-04-06 12:59:21 +0700

    nvim(snippet): move config call to the luasnip side instead

--- home/dot_config/nvim/lua/plugins/snippets.lua
+++ home/dot_config/nvim/lua/plugins/snippets.lua
@@ -2,20 +2,18 @@ return {
   {
     "L3MON4D3/LuaSnip",
     version = "v2.*",
-    dependencies = {
-      {
-        "rafamadriz/friendly-snippets",
-        config = function()
-          local loaders = Utils.lazy_require("luasnip.loaders.from_vscode")
-          loaders.lazy_load()
-          loaders.lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-        end,
-      },
-    },
+    dependencies = { "rafamadriz/friendly-snippets" },
     opts = {
       history = true,
       update_events = { "InsertLeave", "TextChanged", "TextChangedI" },
       delete_check_events = "TextChanged",
     },
+    config = function(_, opts)
+      require("luasnip").setup(opts)
+
+      local loaders = require("luasnip.loaders.from_vscode")
+      loaders.lazy_load()
+      loaders.lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+    end,
   },
 }
