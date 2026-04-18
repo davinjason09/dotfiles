1018b8852ab78602400af47d4bd70810d3ee5e12
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-04-18 23:30:22 +0700

    nvim(lang/cpp): auto semicolon for some keywords

--- home/dot_config/nvim/lua/lang/cpp.lua
+++ home/dot_config/nvim/lua/lang/cpp.lua
@@ -1,10 +1,39 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "c",
-      "cpp",
-      "doxygen",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cpp",
+        "doxygen",
+      },
+    },
+  },
+  {
+    "saghen/blink.pairs",
+    ---@type blink.pairs.Config
+    opts = {
+      mappings = {
+        pairs = {
+          ["{"] = {
+            {
+              "{",
+              "};",
+              when = function(ctx)
+                local text = ctx:text_before_cursor()
+
+                for _, keyword in ipairs({ "class", "struct", "enum", "union" }) do
+                  if text:match(keyword) then return true end
+                end
+
+                return false
+              end,
+              languages = { "cpp" },
+            },
+            { "}", enter = false, space = false },
+          },
+        },
+      },
     },
   },
 }
